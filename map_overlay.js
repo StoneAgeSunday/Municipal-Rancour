0c77510d599f8d66ff91e2520e925f6ca8eb40f7
StoneAgeSunday <ksasarfoabronen@hotmail.com>
2023-07-13 18:30:07 +0200

    joburg is animating

--- map_overlay.js
+++ map_overlay.js
@@ -12,127 +12,188 @@ $(document).ready(function() {
     // Set the custom tile layer as the base layer
     map.addLayer(customTileLayer);
 
-async function loadGeoJSON(){
-    try{
-        const response = await fetch('https://raw.githubusercontent.com/StoneAgeSunday/Municipal-Rancour/main/Maps/geoBoundaries-ZAF-ADM2-all/geoBoundaries-ZAF-ADM2.geojson');
-        const jsonData = await response.json();
+    async function loadGeoJSON() {
+        var municipalLayer;
+        var johannesburgLayer;
+        try {
+            const response = await fetch('https://raw.githubusercontent.com/StoneAgeSunday/Municipal-Rancour/main/Maps/geoBoundaries-ZAF-ADM2-all/geoBoundaries-ZAF-ADM2.geojson');
+            const jsonData = await response.json();
 
-        //create a geoJSON layer and add it to the map
-        L.geoJSON(jsonData, {
-            style: function(feature) {
-                var jhbColour = 'rgb(0, 255, 0)';
-                var defaultColour = 'rgb(0, 0, 255)'
-                if (feature.properties.shapeName === "City of Johannesburg") {
-                    return {
-                        color: jhbColour,  // Set the color to RGB value (0, 255, 0) for City of Joburg
-                        weight: 2,
-                        opacity: 1,
-                        fillColor: jhbColour,
-                        fillOpacity: 0.5
-                    };
-                }
-                else if (feature.properties.shapeName === "City of Tshwane"){
-                    return {
-                        color: jhbColour,  // Set the color to RGB value (0, 255, 0) for City of Joburg
-                        weight: 2,
-                        opacity: 1,
-                        fillColor: jhbColour,
-                        fillOpacity: 0.5
-                    };
-                }
-                else {
-                    return {
-                        color: defaultColour,  // Set the color to RGB value (255, 0, 0) for all other municipalities
-                        weight: 2,
-                        opacity: 1,
-                        fillColour: defaultColour,
-                        fillOpacity: 0.5,
-                    };
-                }
-            }
-        }).addTo(map);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-}
-
-loadGeoJSON();
-});
-    /*
-    const response = await fetch('https://raw.githubusercontent.com/StoneAgeSunday/Municipal-Rancour/main/Maps/geoBoundaries-ZAF-ADM2-all/geoBoundaries-ZAF-ADM2.geojson');
-    const jsonData = await response.json();
-
-    L.geoJSON(jsonData, {
-        style: function(feature) {
-            var jhbColour = 'rgb(0, 255, 0)';
-            var defaultColour = 'rgb(0, 0, 255)'
-            if (feature.properties.shapeName === "City of Johannesburg") {
-                return {
-                    color: jhbColour,  // Set the color to RGB value (0, 255, 0) for City of Joburg
-                    weight: 2,
-                    opacity: 1,
-                    fillColor: jhbColour,
-                    fillOpacity: 0.5
-                };
-            }
-            else if (feature.properties.shapeName === "City of Tshwane"){
-                return {
-                    color: jhbColour,  // Set the color to RGB value (0, 255, 0) for City of Joburg
-                    weight: 2,
-                    opacity: 1,
-                    fillColor: jhbColour,
-                    fillOpacity: 0.5
-                };
-            }
-            else {
-                return {
-                    color: defaultColour,  // Set the color to RGB value (255, 0, 0) for all other municipalities
-                    weight: 2,
-                    opacity: 1,
-                    fillColour: defaultColour,
-                    fillOpacity: 0.5,
-                };
-            }
-        }
-    }).addTo(map); //we need to add updated geoJSON data  at every step of the animation
-    */
-        /*
-        .then(function(data) {
-            // Create a GeoJSON layer and add it to the map
-            L.geoJSON(data, {
+            // Create a geoJSON layer and add it to the map
+            municipalLayer = L.geoJSON(jsonData, {
+                filter: function(feature) {
+                    return feature.properties.shapeName !== "City of Johannesburg";
+                },
                 style: {
-                    color: 'red',  // Customize the boundary line color
-                    weight: 2,     // Customize the boundary line weight
-                    opacity: 1     // Customize the boundary line opacity
+                    color: 'rgb(255, 255, 255)',
+                    weight: 2,
+                    opacity: 1,
+                    fillColor: 'rgb(255, 0, 0)',
+                    fillOpacity: 0.2
                 }
             }).addTo(map);
-        });
-        */
-    /*
-    $.getJSON("//https://github.com/StoneAgeSunday/Municipal-Rancour/blob/main/Maps/geoBoundaries-ZAF-ADM2-all/geoBoundaries-ZAF-ADM2.geojson", function(data) {
-        L.geoJSON(data).addTo(map);
-    });
-    */
-    /*
-    // Example GeoJSON data
-    var geojsonData = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {
-                    "name": "Example Feature"
+
+            // Create the Johannesburg layer
+            johannesburgLayer = L.geoJSON(jsonData, {
+                filter: function(feature) {
+                    return feature.properties.shapeName === "City of Johannesburg";
                 },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [25, -29]
+                style: {
+                    color: 'rgb(255, 255, 255)',
+                    weight: 2,
+                    opacity: 1,
+                    fillColor: 'rgb(255, 255, 255)',
+                    fillOpacity: 0.5
+                }
+            }).addTo(map);
+
+            var startTime = null;
+            var duration = 10000; // 10 seconds
+            var initialColor = 'rgb(255, 255, 255)';
+            var targetColor = 'rgb(0, 255, 0)';
+            var frameNum = 0;
+            var progress = 0;
+            var elapsed = 0;
+
+            async function animateColor(timestamp, initialColor, targetColor) {
+                try{
+                    if (!startTime) {
+                        startTime = timestamp;
+                        progress = 0;
+                    }
+
+                    elapsed = timestamp - startTime;
+                    progress = elapsed / duration;
+
+                    if (progress > 1) {
+                        progress = 1; // Ensure progress doesn't exceed 1
+                    }
+
+                    if (progress < 1) {
+                        var currentColor = getColorTransition(initialColor, targetColor, progress);
+
+                        var johannesburgStyle = {
+                            color: 'rgb(255, 255, 255)',
+                            weight: 2,
+                            opacity: 1,
+                            fillColor: currentColor,
+                            fillOpacity: 0.5
+                        };
+
+                        johannesburgLayer = L.geoJSON(jsonData, {
+                            filter: function(feature) {
+                                return feature.properties.shapeName === 'City of Johannesburg';
+                            },
+                            style: johannesburgStyle
+                        });
+                        map.addLayer(johannesburgLayer);
+                        //johannesburgLayer.addTo(map);
+                        frameNum += 1;
+                        console.log(frameNum.toString() + "th frame about to be drawn");
+                        setTimeout(() => requestAnimationFrame((timestamp) => animateColor(timestamp, initialColor, targetColor)), 1000);
+                    }
+                } catch(error){
+                    console.error('Animation error', error);
                 }
             }
-        ]
-    };
-    */
 
-    // Create a GeoJSON layer and add it to the map
-    //L.geoJSON(geojsonData).addTo(map);
-//https://github.com/StoneAgeSunday/Municipal-Rancour/blob/main/Maps/geoBoundaries-ZAF-ADM2-all/geoBoundaries-ZAF-ADM2.geojson
-//'"C:\Web API Projects\Municipal Rancour\Project\maps\geoBoundaries-ZAF-ADM2-all\geoBoundaries-ZAF-ADM2.geojson"'
+            function startAnimation() {
+                frameNum += 1;
+                console.log(frameNum.toString() + "th frame about to be drawn");
+                setTimeout(() => requestAnimationFrame((timestamp) => animateColor(timestamp, initialColor, targetColor)), 1000);
+            }
+
+            function reset() {
+                startTime = null;
+                //map.clearLayers();
+                // Remove the existing Johannesburg layer from the map
+                /*
+                if (johannesburgLayer) {
+                    map.removeLayer(johannesburgLayer);
+                }
+                */
+                /*
+                map.eachLayer(function(layer){
+                    map.removeLayer(layer);
+                })
+                */
+                map.removeLayer(municipalLayer);
+                municipalLayer = L.geoJSON(jsonData, {
+                    filter: function(feature) {
+                        return feature.properties.shapeName !== "City of Johannesburg";
+                    },
+                    style: {
+                        color: 'rgb(255, 255, 255)',
+                        weight: 2,
+                        opacity: 1,
+                        fillColor: 'rgb(255, 0, 0)',
+                        fillOpacity: 0.2
+                    }
+                }).addTo(map);
+
+
+                // Create the Johannesburg layer
+                johannesburgLayer = L.geoJSON(jsonData, {
+                    filter: function(feature) {
+                        return feature.properties.shapeName === "City of Johannesburg";
+                    },
+                    style: {
+                        color: 'rgb(255, 255, 255)',
+                        weight: 2,
+                        opacity: 1,
+                        fillColor: 'rgb(255, 255, 255)',
+                        fillOpacity: 0.5
+                    }
+                }).addTo(map);
+            }
+
+            function showLayers(){
+                // Iterate over the layers and log their information
+                map.eachLayer(function(layer) {
+                    //console.log(layer); // Output the layer object to the console
+
+                    // You can access specific properties of the layer if needed
+                    //console.log(layer.options); // Output the options/settings of the layer
+                    //console.log(layer.getAttribution()); // Output the attribution of the layer
+                    console.log(layer.style);
+                });
+            }
+
+            // Start the animation when a button is clicked, for example
+            document.getElementById('start-animation-button').addEventListener('click', startAnimation);
+
+            document.getElementById('reset-button').addEventListener('click', reset);
+
+            document.getElementById('show-layers-button').addEventListener('click', showLayers);
+
+        } catch (error) {
+            console.error('Main error:', error);
+        }
+    }
+
+    loadGeoJSON();
+
+    function getColorTransition(initialColor, endColor, progress) {
+        var initArr = parseRGB(initialColor);
+        var endArr = parseRGB(endColor);
+
+        var r = Math.round(initArr[0] + ((endArr[0] - initArr[0]) * progress));
+        var g = Math.round(initArr[1] + ((endArr[1] - initArr[1]) * progress));
+        var b = Math.round(initArr[2] + ((endArr[2] - initArr[2]) * progress));
+
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+
+    function parseRGB(colorString) {
+        const regex = /rgb\((\d+),\s*(\d+),\s*(\d+)\)/;
+        const matches = colorString.match(regex);
+
+        if (matches) {
+            const [, red, green, blue] = matches;
+            return [parseInt(red), parseInt(green), parseInt(blue)];
+        }
+
+        return null; // Return null if the colorString does not match the expected format
+    }
+
+});
